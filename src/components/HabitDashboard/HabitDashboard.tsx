0fa36082accb6331dffff32e40c8ab192ef2b5c6
Peter Nguyen <peternguyen008@gmail.com>
2023-03-13 18:09:00 -0400

    initial commit - created HabitDashboard

---
+++ src/components/HabitDashboard/HabitDashboard.tsx
@@ -0,0 +1,39 @@
+// import React, { useState } from 'react';
+// import HabitsContainer from '../HabitsContainer';
+// import NewHabitForm from '../NewHabitForm';
+import { Habit } from '../../App';
+
+// export interface Habit {
+// id: number,
+// title: string,
+// daysTracked: number,
+// description: string,
+// updated_at: string,
+// reminderTime: string,
+// dayCount: number,
+// logs: Log[]
+// }
+
+function HabitDashboard(props: { data: Habit }) {
+    const { data } = props;
+    console.log(data)
+  
+  
+    return (
+      <div className="HabitDashboard">
+        <div className="habitContainer">
+          <p className="habitName">[habit.title]</p>
+          <p className="habitTimeline">[habit.daysTracked]</p>
+        </div>
+  
+        {/* {habits.log.map((dayLog) => {
+            // log each day
+        })} */}
+        
+        {/* <LogHabitForm /> */}
+      </div>
+    );
+  }
+  
+  
+  export default HabitDashboard;
