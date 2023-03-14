d50fb144ea54df2d4bcfe727a792c16a3e12738d
Nicholas Mendez <nicholasmendez@Nicholass-MacBook-Pro.local>
2023-03-14 08:59:00 -0400

    created input styles for log form

--- src/components/Log/LogForm.tsx
+++ src/components/Log/LogForm.tsx
@@ -1,26 +1,4 @@
-import { LogFormRoot, FormWrapper, TextInput, NumberInput, FormButton } from "./LogForm.styles"
-import usePostLogs from "../../hooks/usePostLogs"
-
-import { useFormik } from "formik"
-import * as Yup from 'yup'
-import { Log } from "../../App"
-// {
-//   "exercise": "Squats",
-//   "weight": 50,
-//   "reps": 10,
-//   "sets": 3,
-// }
-
-// id: number, 
-// exercise: string,
-// created_at: string,
-// weight: number, 
-// reps: number,
-// habitId: number, 
-// sets: number, 
-// total: number,
-// completed: boolean
-
+import { LogFormRoot, FormWrapper } from "./LogForm.styles"
 
 const LogForm = () => {
   const submitLogMutation = usePostLogs()
@@ -47,48 +25,8 @@ const LogForm = () => {
 
 return  (
   <LogFormRoot>
-    <FormWrapper id="log-form" onSubmit={formik.handleSubmit}>
-      <label>
-        Exercise 
-        <TextInput 
-          id="exercise"
-          placeholder="Exercise name here..."
-          type="text"
-          onChange={formik.handleChange}
-          />
-      </label>
-      <label>
-        Weight 
-        <NumberInput
-          id="weight"
-          type="number"
-          min={0}
-          onChange={formik.handleChange}
-          />
-      </label>
-      <label>
-        Reps 
-        <NumberInput
-          id="reps"
-          type="number"
-          min={0}
-          // value={formik.initialValues.reps}
-          onChange={formik.handleChange}
-        />
-      </label>
-      <label>
-        Sets
-        <NumberInput
-          id="sets"
-          type="number"
-          min={0}
-          onChange={formik.handleChange}
-        />
-      </label>
-      <FormButton
-        form="log-form"
-        type="submit"
-      >Submit</FormButton>
+    <FormWrapper>
+      
     </FormWrapper>
   </LogFormRoot>
 )
