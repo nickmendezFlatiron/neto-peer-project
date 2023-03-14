7afd02454e26529ffe0093330c6c88480fbb7eef
Nicholas Mendez <nicholasmendez@Nicholass-MacBook-Pro.local>
2023-03-14 09:16:38 -0400

    created button styled component and imported yup,formik to log-form component

--- src/components/Log/LogForm.styles.ts
+++ src/components/Log/LogForm.styles.ts
@@ -8,8 +8,21 @@ justify-content: center;
 `
 
 export const FormWrapper = styled.form`
+padding: 20px;
+`
+
+export const TextInput = styled.input`
 
 `
 
 export const NumberInput = styled.input`
+margin: 10px 5px; 
+`
+
+export const TextLabel = styled.label`
+
+`
+export const FormButton = styled.button`
+align-self: center;
+
 `
--- src/components/Log/LogForm.tsx
+++ src/components/Log/LogForm.tsx
@@ -1,4 +1,21 @@
-import { LogFormRoot, FormWrapper } from "./LogForm.styles"
+import { LogFormRoot, FormWrapper, TextInput, NumberInput } from "./LogForm.styles"
+import { useFormik } from "formik"
+import * as Yup from 'yup'
+
+// {
+//   "exercise": "Squats",
+//   "weight": 50,
+//   "reps": 10,
+//   "sets": 3,
+// }
+
+// id="firstName" 
+// type="text"
+// placeholder="first name here..."
+// onChange={formik.handleChange}
+// onBlur={formik.handleBlur}
+// value={formik.values.firstName}
+
 
 const LogForm = () => {
   const submitLogMutation = usePostLogs()
@@ -26,7 +43,13 @@ const LogForm = () => {
 return  (
   <LogFormRoot>
     <FormWrapper>
-      
+      <label>
+        Exercise 
+        <TextInput 
+          id="exercise"
+          placeholder="Exercise name here..."
+          type="text"/>
+      </label>
     </FormWrapper>
   </LogFormRoot>
 )
