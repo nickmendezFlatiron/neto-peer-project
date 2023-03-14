fc7df2f4e91a443f8e66e6077b010da9e4bd92af
Nicholas Mendez <nicholasmendez@Nicholass-MacBook-Pro.local>
2023-03-14 08:59:00 -0400

    created input styles for log form

--- src/components/Log/LogForm.styles.ts
+++ src/components/Log/LogForm.styles.ts
@@ -3,8 +3,17 @@ import styled from "styled-components";
 export const LogFormRoot = styled.div`
 border-style: solid;
 border-width: 4px;
+display: flex;
+justify-content: center;
 `
 
 export const FormWrapper = styled.form`
+padding: 20px;
+`
 
+export const TextInput = styled.input`
+
+`
+
+export const NumberInput = styled.input`
 `
--- src/components/Log/LogForm.tsx
+++ src/components/Log/LogForm.tsx
@@ -1,10 +1,33 @@
-import { LogFormRoot, FormWrapper } from "./LogForm.styles"
+import { LogFormRoot, FormWrapper, TextInput, NumberInput } from "./LogForm.styles"
+import { useFormik } from "formik"
+import * as Yup from 'yup'
+
+// {
+//   "exercise": "Squats",
+//   "weight": 50,
+//   "reps": 10,
+//   "sets": 3,
+// }
+
+// id="firstName" 
+// type="text"
+// placeholder="first name here..."
+// onChange={formik.handleChange}
+// onBlur={formik.handleBlur}
+// value={formik.values.firstName}
+
 
 const LogForm = () => {
 return  (
   <LogFormRoot>
     <FormWrapper>
-      
+      <label>
+        Exercise 
+        <TextInput 
+          id="exercise"
+          placeholder="Exercise name here..."
+          type="text"/>
+      </label>
     </FormWrapper>
   </LogFormRoot>
 )
