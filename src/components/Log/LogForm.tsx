060853e3a51b888c8db0b84834b5771edd5c4c70
Nicholas Mendez <mendez.nicholasr@gmail.com>
2023-03-16 11:48:55 -0400

    copy logform component from log-form branch into routes branch

--- src/components/Log/LogForm.tsx
+++ src/components/Log/LogForm.tsx
@@ -1,7 +1,8 @@
 import { LogFormRoot, FormWrapper, TextInput, NumberInput, FormButton } from "./LogForm.styles"
-import { useFormik } from "formik"
+import { useFormik, validateYupSchema } from "formik"
+import usePostLogs from "../../hooks/usePostLogs"
 import * as Yup from 'yup'
-import { Log } from "../../types"
+
 // {
 //   "exercise": "Squats",
 //   "weight": 50,
@@ -19,29 +20,67 @@ import { Log } from "../../types"
 // total: number,
 // completed: boolean
 
+interface LogFormInputs {
+  exercise: string,
+    weight: number,
+    reps: number,
+    sets: number
+}
 
 const LogForm = () => {
+
+  const submitLogMutation = usePostLogs()
+
+  const SignupSchema = Yup.object().shape({
+    exercise: Yup.string()
+      .min(2, 'Too Short!')
+      .max(30, 'Too Long!')
+      .required('Required'),
+    weight: Yup.number()
+    .min(0)
+    .required('Required'),
+    reps: Yup.number()
+    .min(0)
+    .required('Required'),
+    sets: Yup.number()
+    .min(0)
+    .required('Required')
+  });
 
   const formik = useFormik({initialValues: {
     exercise: '',
     weight: 0,
     reps: 0,
     sets: 0
-  } ,  onSubmit: (values) => {
-    alert(JSON.stringify(values, null, 2));
+  }, 
+  // validateYupSchema: SignupSchema,  
+  onSubmit: (values) => {
+    const { reps, weight, sets } = values
+    const total = reps * weight * sets
+
+    const logData = {
+      ...values,
+      habitId: 0,
+      total,
+      created_at: Date.now().toString(),
+      completed: true
+
+    }
+    submitLogMutation.mutate(logData)
   }})
 
 return  (
   <LogFormRoot>
-    <FormWrapper id="log-form">
+    <FormWrapper id="log-form" onSubmit={formik.handleSubmit}>
       <label>
         Exercise 
         <TextInput 
           id="exercise"
           placeholder="Exercise name here..."
           type="text"
-          // value={formik.initialValues.exercise}
           onChange={formik.handleChange}
+          value={formik.values.exercise}
+          maxLength={30}
           />
       </label>
       <label>
@@ -50,28 +89,34 @@ return  (
           id="weight"
           type="number"
           min={0}
-          // value={formik.initialValues.weight}
           onChange={formik.handleChange}
+          value={formik.values.weight}
           />
       </label>
       <label>
         Reps 
         <NumberInput
-          id="weight"
+          id="reps"
           type="number"
           min={0}
+          onChange={formik.handleChange}
+          value={formik.values.reps}
         />
       </label>
       <label>
         Sets
         <NumberInput
-          id="weight"
+          id="sets"
           type="number"
           min={0}
-          // value={formik.initialValues.sets}
           onChange={formik.handleChange}
+          value={formik.values.sets}
         />
       </label>
+      <FormButton
+        form="log-form"
+        type="submit"
+      >Submit</FormButton>
     </FormWrapper>
   </LogFormRoot>
 )
