e8edf28ba2650c9a7b9a61ffee7f3163890aca00
Nicholas Mendez <mendez.nicholasr@gmail.com>
2023-03-16 12:18:49 -0400

    import Log and Habit interface from types file and handle log form submit logic

--- src/hooks/usePostLogs.tsx
+++ src/hooks/usePostLogs.tsx
@@ -1,5 +1,6 @@
 import {useMutation} from '@tanstack/react-query'
-import { Log } from '../types'
+import { Habit, Log } from '../types'
+import { useQueryClient } from '@tanstack/react-query'
 interface LogData {
 exercise: string,
 created_at: string,
@@ -11,24 +12,44 @@ total: number,
 completed: boolean
 }
 
-const postLogData = async (data: LogData): Promise<Log> => {
-  const res = await fetch('http://localhost:3000/logs', {
-    method: "POST",
-    body: JSON.stringify(data),
-    headers: {
-      "Content-Type": "application/json"
+
+const postLogData = async (data: LogData) => {
+
+  try {
+    const res = await fetch('http://localhost:3000/logs', {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+    // if (!res.ok) {
+    //  return alert("Error: Log was unsuccessfully created")
+    // }
+    if (res.ok) {
+      return res.json()
     }
-  })
-  return res.json()
+
+  } catch {
+
+  }
 }
 
 const usePostLogs = () => {
+const client = useQueryClient({})
+
  return useMutation({
     mutationFn: postLogData, 
-    onSuccess: (data) => {console.log(data)}
+    onSuccess: (data: Log ) => {
+      console.log("success")
+      const allHabits: any = client.getQueryData(['habits'])
+      const findHabit = allHabits.find((habit: Habit) => habit.id === data.habitId)
+      findHabit.logs.push(data)
+      return findHabit
+    },
+    onError: ( ) => {
+      alert("Error: Log was unsuccessfully created")
+    }
   })
  
 }
-
-
-export default usePostLogs
